a8a5ccfe59981e7da332e3e89aa992e14e63f30d
castlehyeon <qnftktls118@naver.com>
2022-07-21 01:34:05 +0900

    4번 풀이

--- singleton0720/src/main/java/com/dev_sapjil/사람.java
+++ singleton0720/src/main/java/com/dev_sapjil/사람.java
@@ -18,27 +18,44 @@ package com.dev_sapjil;
 //    }
 //}
 
+import java.util.ArrayList;
+//
+//1. 최대 사람수 x  -> 3
+//2. a사람x -> arraylist 저장소
 class 사람 {
     static private int 사람수;
     //힙에 먼저 올라간다.
     private int 번호;
     //지역변수
-
+    static ArrayList<사람> arr;
 
     // static 요소 전용 생성자
     // 따로 뭔가를 호출하지 않아도 프로그램이 실행되면 가장먼저 실행된다.
     static {
         사람수 = 0;
+        arr = new ArrayList<>();
     }
     //힙에 올라갈 때 초기화
     private 사람(int 번호) {
         this.번호 = 번호;
+
     }
     //이건 일반적인 생성자: 매개변수 있음!
-    static 사람 get사람() {
-        사람 a사람 = new 사람(사람수 + 1);
-        //new 사람하면서, ()안에 스태틱변수를 1씩 추가-> 이게 번호로 저장
-        사람수++;
+    static 사람 get사람() {//스태틱메서드 안에 인스턴스 변수를 못씀.
+            사람 a사람 = new 사람(사람수 + 1); //a사람=지역변수, static?
+        //a사람은 지역변수
+        //a사람은 지금 new 사람(index번호) 객체가 생성될때마다, 그 주소값을 저장한다.
+            arr.add(사람수, a사람);
+            //추가, 삭제, 수정 add, remove, set
+            //new 사람하면서, ()안에 스태틱변수를 1씩 추가-> 이게 번호로 저장
+
+        //안없어지는 저장소? -> static선언한 arraylist라는 거죠!!!!
+        //arraylist는 배열, 배열 각각에다가 저는 객체의 주소를 넣었어요!!
+        ++사람수;
+        if( 사람수  > 3){  ///if문
+                return arr.get(2);//이게 지금 제일 마음에 안듦?? ->>>>>>>> 2가 들어가야대!!
+           }
+
         //후입 선입은 크게 상관없는듯.
         return a사람;
     }
