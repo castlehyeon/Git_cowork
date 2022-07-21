951b3ffedb69a6d9074cadf0dca127b67e7ec5d8
chae-hwa <ihj0519@naver.com>
2022-07-22 03:36:46 +0900

    옵저버패턴 6번문제 해결

--- observerpattern/src/main/java/com/dev_chae_hwa/Main.java
+++ observerpattern/src/main/java/com/dev_chae_hwa/Main.java
@@ -17,37 +17,44 @@ class Main {
         aButton.fireClick();
     }
 }
-class Button{
-    private Object animals;
 
-    public void setClickEventListener(Object animals) {
+class Button{
+    동물 animals;
+
+    public void setClickEventListener(동물 animals) {
         this.animals = animals;
     }
 
-    public void fireClick() { //object로 받아온 건 리모컨 버튼을 제거하는 것.
-        if(animals instanceof 고양이){ //animals은 고양이, 개, 부엉이 중 무엇이냐 물은 후,
-            ((고양이)animals).고양이응답(); //그에 맞는 동물로 형변환 및 메서드 호출
-        }
-        if(animals instanceof 개){
-            ((개)animals).개응답();
-        }
-        if(animals instanceof 부엉이){
-            ((부엉이)animals).부엉이응답();
-        }
+    public void fireClick() {
+        animals.알림();  // 각각의 객체가 담긴 animals 리모컨이 알림 버튼을 작동시킨다.
     }
 }
-class 고양이{
+interface 동물{
+    void 알림();  // 인터페이스의 추상메서드는 선언부만 작성한다. 자식에게 구현을 맡김.
+}
+class 고양이 implements 동물{
     void 고양이응답(){
-        System.out.println("나(고양이)는 방금 버튼이 클릭되었다는 사실을 전달받았습니다.");
+        System.out.println("나(고양이)는 방금 버튼이 클릭되었다는 사실을 전달 받았습니다.");
+    }
+    public void 알림(){ // 인터페이스의 메서드를 받아올 땐 무조건 public, 그리고 구현부까지 작성한다.
+        고양이응답();
     }
 }
-class 개{
+class 개 implements 동물{
     void 개응답(){
-        System.out.println("나(개)는 방금 버튼이 클릭되었다는 사실을 전달받았습니다.");
+        System.out.println("나(개)는 방금 버튼이 클릭되었다는 사실을 전달 받았습니다.");
     }
+    public void 알림(){
+        개응답();
+    }
+
 }
-class 부엉이{
+class 부엉이 implements 동물{
     void 부엉이응답(){
-        System.out.println("나(부엉이)는 방금 버튼이 클릭되었다는 사실을 전달받았습니다.");
+        System.out.println("나(부엉이)는 방금 버튼이 클릭되었다는 사실을 전달 받았습니다.");
     }
+    public void 알림(){
+        부엉이응답();
+    }
+
 }
