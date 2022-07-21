53e338b1c193718c444a48748470d1b02b78fce4
castlehyeon <qnftktls118@naver.com>
2022-07-21 11:43:05 +0900

    옵저버패턴6번

--- observerpattern/src/main/java/com/dev_sapjil/Main.java
+++ observerpattern/src/main/java/com/dev_sapjil/Main.java
@@ -8,7 +8,6 @@ class Main {
     public static void main(String[] args) {
         Button aButton = new Button();
         aButton.setClickEventListener(new 고양이());
-        //객체 생성
         aButton.fireClick();
         // 나(고양이)는 방금 버튼이 클릭되었다는 사실을 전달 받았습니다.
 
@@ -23,57 +22,53 @@ class Main {
     }
 }
 class Button{
-    Object info;
     Animal animal;
-
-
     public Button(){
-        info = null;
         animal = null;
-        //주소값이 들어가는 animal
     }
-    public void setClickEventListener(Object info) {
-        this.info = info;
-        //해당 동물객체로 다운캐스팅
+    public void setClickEventListener(Animal info) {
+        animal = info;
     }
 
     public void fireClick() {
-        //animal = (Animal) info;
-        //System.out.println("나("+animal.name+")는 방금 버튼이 클릭되었다는 사실을 전달 받았습니다.");
-        //캐스팅이 안된다. 고양이, 개, 부엉이 타입 -> Object -> Animal로 다운캐스팅이 불가능.
-        System.out.println("나("+info+")는 방금 버튼이 클릭되었다는 사실을 전달 받았습니다.");
-        //단순히 주소값은 받아오는 것 가능.
-        //animal.name을 어떻게든 긁어오고 싶었는데...
-        //현재는 name을 선언한 의미가 없다.
-
+        animal.response();
     }
 }
-class Animal{
-    public String name;
-    public Animal(){
-        name = "";
+interface Animal{
+    //Listener의 역할
+    void response();
+    //응답의 역할
+}
+class 고양이 implements Animal{
+    void 울음소리(){
+        System.out.println("나(고양이)는 방금 버튼이 클릭되었다는 사실을 전달 받았습니다.");
+    }
+    @Override
+    public void response() {
+        울음소리();
     }
 }
-class 고양이{
-    //String name;
-    Animal animal;
-    public 고양이(){
-        //name = String.valueOf(new Animal());
-        animal = new Animal();
-        animal.name = "고양이";
+class 개 implements Animal{
+    void 울음소리(){
+        System.out.println("나(개)는 방금 버튼이 클릭되었다는 사실을 전달 받았습니다.");
+    }
+    @Override
+    public void response() {
+        울음소리();
     }
 }
-class 개{
-    Animal animal;
-    public 개(){
-        animal = new Animal();
-        animal.name = "개";
+class 부엉이 implements Animal{
+    void 울음소리(){
+        System.out.println("나(부엉이)는 방금 버튼이 클릭되었다는 사실을 전달 받았습니다.");
+    }
+    @Override
+    public void response() {
+        울음소리();
     }
 }
-class 부엉이{
-    Animal animal;
-    public 부엉이(){
-        animal = new Animal();
-        animal.name = "부엉이";
-    }
-}
+//인터페이스를 선언하는 것까지는 알겠어.
+//그럼 버튼을 눌렀을 때, 무엇이 실행되야할까.
+//1. Button측에 무엇을 만들어야 출력이 될 것이다.
+//2. 리스너 타입의 a리스너를 만든다. 주소값을 담기 위한 null초기화
+//3. 어차피 매개변수로 들어오는 것은 리스너를 상속받았기 때문에 매개변수타입을 리스너로 했다. -> Object 타입선언당시의 캐스팅에러 해결
+//4. 주소값을 해당 a리스너에 넣고, 메서드가 실행될 때마다 리스너에 정의된 메서드를 호출한다.
