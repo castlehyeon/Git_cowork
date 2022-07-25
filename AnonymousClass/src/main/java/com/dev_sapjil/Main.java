f1a8a0b59ab0c720b3593f41a98c508ec3e046b9
castlehyeon <qnftktls118@naver.com>
2022-07-25 10:21:18 +0900

    Inner Class2

--- AnonymousClass/src/main/java/com/dev_sapjil/Main.java
+++ AnonymousClass/src/main/java/com/dev_sapjil/Main.java
@@ -6,20 +6,18 @@ class Main {
 
         // 수정가능지역 시작
 
-        //방법 1. 익명클래스는 아니다.
-        class 방법1 implements View.OnClickListener{
-            public void onClick(){
-                System.out.println("클릭되었다는 사실을 전달받았습니다.");
-            }
-        }
-
-        aOnClickListener = new 방법1();
-        // 수정가능지역 끝
-
+        //방법 2. 익명클래스
+        aOnClickListener = new View.OnClickListener(){
+                public void onClick(){
+                    System.out.println("클릭되었다는 사실을 전달받았습니다.");
+                }
+            };
         aOnClickListener.onClick();
         // 출력 => 클릭되었다는 사실을 전달받았습니다.
     }
-}
+    // 수정가능지역 끝
+
+    }
 
 class View {
     static interface OnClickListener {
